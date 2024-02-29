9ec920aef44a9f8662548b545889bdd24b3583f9
p.bogusz <p.bogusz@cheil.com>
2024-02-29 21:30:08 +0100

    Initial setup

---
+++ movies-app-vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+$bg-color: #f5f5f5;
+</style>
