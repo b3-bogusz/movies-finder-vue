dd5dabb9514dfab48925abfedcbb6b1efef2682b
p.bogusz <b3.bogusz@gmail.com>
2024-03-08 11:22:30 +0100

    add Logo component

---
+++ movies-app-vue/src/components/Logo/Logo.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="logo">
+    <img src="@/assets/img/main_logo.png" alt="App logo" class="logo__img" />
+  </div>
+</template>
+
+<style lang="scss" scoped src="./Logo.scss"></style>
--- movies-app-vue/src/views/HomeView.vue
+++ movies-app-vue/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import MoviesList from '@/modules/MoviesList/MoviesList.vue'
 import TypeHeader from '@/components/TypeHeader/TypeHeader.vue'
+import Logo from '@/components/Logo/Logo.vue'
 </script>
 
 <template>
   <main>
+    <Logo />
     <TypeHeader
       title="Your Movies"
       subtitle="Below you can find the movies that came back from your search"
